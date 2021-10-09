966f7dd0d805fac1b4ea3b716cd34379129ec6ef
kperkins97 <perkins.kelsey@yahoo.com>
2021-10-09 09:41:28 -0500

    adjusting navbar

--- assets/js/screen2.js
+++ assets/js/screen2.js
@@ -1,13 +1,13 @@
 var ApiKey='ac6ee46d74fff945f4929327853f7573';
-var today = moment().format('MMMM Do, YYYY, hh:mm A'); 
+var today = moment().format('MMM. Do, YYYY - hh:mm A'); 
 $('#currentDay').text(today);
 var tempEl = document.querySelector('#temp');
 var windspeedEl = document.querySelector('#windspeed');
 var humidityEl = document.querySelector('#humidity');
-
+var title = document.getElementById('#title');
 
 var getAtlantaWeather = function (Atlanta) {
-  var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q='  + Atlanta + '&appid=' + ApiKey + '&units=imperial';
+  var weatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Atlanta&appid=' + ApiKey + '&units=imperial';
   console.log(getAtlantaWeather);
 
 
@@ -19,8 +19,9 @@ var getAtlantaWeather = function (Atlanta) {
   .then(function (data) {
           console.log(data);
           //should display weather elements
-          tempEl.textContent = data.main.temp + ' °F';
-          humidityEl.textContent = data.main.humidity + '%'
-          windspeedEl.textContent = data.wind.speed + ' mph';
+          tempEl.textContent = ' ' + data.main.temp + ' °F';
+          humidityEl.textContent = ' ' + data.main.humidity + '%'
+          windspeedEl.textContent = ' ' + data.wind.speed + ' mph';
         });
 };
+getAtlantaWeather();
